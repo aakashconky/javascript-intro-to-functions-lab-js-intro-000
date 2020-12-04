339f62756db3a0e11c3c90089e2c7553f903fb0c
aakash thapa <aakash_conky@hotmail.com>
2020-12-04 06:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function shout(string) {
   retrurn string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+ console.log(string.toLowerCase())
+ }
+ 
